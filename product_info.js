0eef06591b938a70ddfda2da8079367d3220a1da
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-11 12:56:01 +0530

    object class excess, practice questions

---
+++ product_info.js
@@ -0,0 +1,10 @@
+//refer from the video number 1 practice question
+
+const product ={
+    title: "Ball Pen",
+    rating: 4,
+    offer: 5,
+    price: 270
+};
+console.log(product);
+
---
+++ profile_info.js
@@ -0,0 +1,9 @@
+const profile={
+    username  : "@shradhakhapra",
+    isfollow :true,
+    posts :195,
+    followers : 569,
+    following : 4,
+};
+
+console.log(typeof profile["isfollow"]);
