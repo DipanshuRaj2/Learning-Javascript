dc7845ecd2865f4213240f662765d34bbfa8a19a
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-12 11:32:52 +0530

    operators and conditional statement

---
+++ Operator-and-Conditional/script.js
@@ -0,0 +1,23 @@
+/*let a = 5;
+let b = 2;
+
+// aritmetic operator
+console.log("a = ", a, "& b = ",b);
+console.log("a + b = ", a+b);
+console.log("a - b = ", a-b);
+console.log("a * b = ", a*b);
+console.log("a / b = ", a/b);
+console.log("a % b = ", a%b);
+console.log("a ** b = ", a**b);//5^2
+*/
+//Unary Operator
+let a = 5;
+let b = 2;
+
+console.log("a = ",a, " & b = ",b);
+a++; //6
+
+console.log("a = ", a); //6
+b--;
+
+console.log("b = ", b); //6
