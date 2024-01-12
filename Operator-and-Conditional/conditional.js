e55a6d40833ccaa7780b89a25aed6612c5bf8a3f
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-12 12:59:30 +0530

    conditional if, if else, else if

---
+++ Operator-and-Conditional/conditional.js
@@ -0,0 +1,58 @@
+//conditional statement
+let age = 12;
+if(age>18){
+    console.log("You can Vote");
+}
+else{
+    console.log("you CANNOT vote");
+}
+/*
+let mode = "light";
+let color;
+
+if(mode === "dark"){
+    color = "black";
+}
+if(mode === "light"){
+    color = "white";
+}
+console.log(color);
+*/
+
+// let mode = "dark";
+// let color;
+
+// if(mode == "dark"){
+//     color ="black";
+// } else {
+//     color = "white";
+// }
+// console.log(color);
+
+// //find the no even or odd
+// let num = 17;
+// if(num %2 === 0){
+//     console.log("even");
+// }
+// else{
+//     console.log("odd");
+// }
+
+let mode = "black";
+let color;
+
+if(mode === "dark"){
+    color = "black";
+}
+else if(mode === "blue"){
+    color = "blue";
+}
+else if(mode === "pink"){
+    color = "white";
+}
+else{
+    color = "white";
+}
+console.log(color);
+
+//
