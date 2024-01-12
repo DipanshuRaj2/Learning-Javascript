c08e8348d970b94768bd522debd6d61af86e3210
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-12 12:17:15 +0530

    conditiona

---
+++ Operator-and-Conditional/conditionalOpertor.js
@@ -0,0 +1,21 @@
+//conditional statement
+// let age = 12;
+// if(age>18){
+//     console.log("You can Vote");
+// }
+// if(age < 18){
+//     console.log("you CANNOT vote");
+// }
+/*
+let mode = "light";
+let color;
+
+if(mode === "dark"){
+    color = "black";
+}
+if(mode === "light"){
+    color = "white";
+}
+console.log(color);
+
+*/
--- Operator-and-Conditional/script.js
+++ Operator-and-Conditional/script.js
@@ -10,14 +10,43 @@ console.log("a / b = ", a/b);
 console.log("a % b = ", a%b);
 console.log("a ** b = ", a**b);//5^2
 */
+
+/*
 //Unary Operator
 let a = 5;
 let b = 2;
 
 console.log("a = ",a, " & b = ",b);
-a++; //6
 
-console.log("a = ", a); //6
-b--;
+console.log("a++ = ", a++ );//5
+console.log("++a = ", ++a );//7
+console.log("a = ", a );//7
 
-console.log("b = ", b); //6
+
+
+
+console.log("b = ", b--) //2
+console.log("b = ", --b); //0
+console.log("b = ", b); //0
+*/
+
+//Assignment Operator
+// let a = 5;
+// let b = 2;
+
+// a **=4; // a = a*4;
+
+// console.log("a = ", a);
+
+/*
+comparison operator
+greater than 
+less than
+boolean value return
+strict not equal to ===(based on the data types)
+let a  = 5;
+let b = "5";
+console.log("5 == 5 ", a===b);//true
+*/
+
+//logical operator (and ,or,logical NOT) 
