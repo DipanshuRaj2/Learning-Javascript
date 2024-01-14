27a62e03c20a93fbb11d627540cd3f63528f6562
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-15 01:16:50 +0530

    string practice

--- Loops And Strings/practice.js
+++ Loops And Strings/practice.js
@@ -7,10 +7,21 @@
 
 // Q2 Create a game where you start with any random game number. Ask the user to keep guessing the game number until the user enters the correct value.
 
-let gameNum = 25; 
-let userNum = prompt("Guess the game number : ");
+// let gameNum = 25; 
+// let userNum = prompt("Guess the game number : ");
 
-while(userNum !=gameNum){
-    userNum =prompt("You entered wrong number. Guess again : ");
-}
-console.log("congratualtions, you entered the right number");
+// while(userNum !=gameNum){
+//     userNum =prompt("You entered wrong number. Guess again : ");
+// }
+// console.log("congratualtions, you entered the right number");
+
+
+/*Qs1 Prompt the user to enter their full name. Generate a username for them based on the input.
+start username with @, followed by their full name and ending with the fullname length.
+
+eg : user name ="dipanshuraj", username should be "@dipanshuraj12"
+*/
+
+let userName = prompt("Enter the full name");
+console.log("@"+userName+userName.length);
+
--- Loops And Strings/strings.js
+++ Loops And Strings/strings.js
@@ -25,10 +25,45 @@ String Interpolation
 
 // console.log(`the cost of ${obj.item} is ${obj.price} rupees`);
 
-let specialString = `This is a template literal ${1+2+3}`;
-console.log(specialString);
+// let specialString = `This is a template literal ${1+2+3}`;  
+// console.log(specialString);
 
-let str =`Dipanshu \nRaj`;
-console.log(str);
-let str1 =`vandana\tdash`;
-console.log(str1);
+// let str =`Dipanshu \nRaj`;
+// console.log(str);
+// let str1 =`vandana\tdash`;
+// console.log(str1);
+// console.log(str1.length);
+
+//String Methods in java 
+//. javascript ke ander string immutable(changes nahi hota) hoti hain 
+//. matlab ki ek baar value assign kia hain toh change nhi hoga 
+//. 1 toUppercase()
+// let str = "dipanshu raj";
+// let str1 = str.toUpperCase();
+
+// console.log(str);
+// console.log(str1);
+
+//2 toLowerCase
+// let str = "DIPANSHURAJ";
+// let str1 = str.toLowerCase();
+// console.log(str);
+// console.log(str1);
+
+//3. trim() startng or end se white space ko erase kar deta hain
+
+// let str = "  Apna College JS  ";
+// console.log(str.trim());
+
+
+ //4. slice() method //str.slice(1, 4) 
+// let str = "DipanshuRaj";
+// console.log(str.slice(1, 4));
+
+//5. concat()
+
+// let str = "DipanshuRaj";
+// let str1 = "Chittu";
+// console.log(str.concat(str1));
+
+
