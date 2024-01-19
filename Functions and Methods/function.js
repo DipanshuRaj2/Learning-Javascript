3315e9bd08fd05dc8c3e1b8eb95a5f9dbe95604d
DipanshuRaj2 <dipanshurajjmt2@gmail.com>
2024-01-19 23:20:17 +0530

    function

--- Functions and Methods/function.js
+++ Functions and Methods/function.js
@@ -27,19 +27,43 @@
 //compact way of writing a function
 
 //sum Arrow function
-const arrowSum =(a,b) =>{
-    console.log(a+b);
-}
-arrowSum(23 , 54);
+// const arrowSum =(a,b) =>{
+//     console.log(a+b);
+// }
+// arrowSum(23 , 54);
 
 //mutilply Arrow function
-const multi =(a , b)=>{
-    return (a*b);
-}
+// const multi =(a , b)=>{
+//     return (a*b);
+// }
 
-console.log(multi(12, 9));
+// console.log(multi(12, 9));
 
-const printHello = () =>{
-    console.log("hello");
-};
-printHello();
+// const printHello = () =>{
+//     console.log("hello");
+// };
+// printHello();
+
+//callbackFunction : Here, it is a function to execute for each element to another function.
+// for each loop 
+// let arr = [1,2,3,4,5];
+// arr.forEach(function printVal(val){
+//     console.log(val);
+// });
+
+//call back (forEach) using arrowfunction
+
+// let arr = [1,2,3,4,5];
+// arr.forEach((val)=>{
+//     console.log(val);
+// });
+
+// print the city name 
+let arr = ["pune","delhi","mumbai"];
+arr.forEach((val, idx)=>{
+    console.log(val.toUpperCase(), idx);
+}); 
+
+
+
+
