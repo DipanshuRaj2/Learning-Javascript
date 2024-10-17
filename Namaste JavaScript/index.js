67364f81f6802bce68d149eefb16c11c21141844
dipkali <dipkali@kali.dipkali>
2024-10-17 14:27:09 +0530

    add event listener

--- Namaste JavaScript/index.js
+++ Namaste JavaScript/index.js
@@ -1,9 +1,42 @@
 console.log("Radhe Radhe");
 
-let x = 16;
-x = squareOfx(x);
-console.log(`Square of x:  ${x}`);
+// let x = 16;
+// x = squareOfx(x);
+// console.log(`Square of x:  ${x}`);
+// console.log("square of x: "+ x);  //both of the correct way to print the o/p;
 
-function squareOfx(x){
-    return x*x;
-}
+// function squareOfx(x){
+//     return x*x;
+// }
+
+
+// function x(){
+//     console.log(`x`);
+//     y();
+// }
+// function y(){
+//     console.log(y);
+// }
+
+// call back function
+
+// setTimeout(function() {
+//     console.log("timer")
+// } , 2000);
+
+
+// function x(y){
+//     console.log("x");
+//     y();
+// }
+
+// x(function y(){
+//     console.log("y");
+// })
+const button = document.getElementById('button');
+button.addEventListener("click", function(){
+    console.log("hello world");
+})
+
+
+
